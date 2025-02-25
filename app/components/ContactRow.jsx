8f80abafc4fdb213cbc50f9ad3e5175115d74cf3
Christopher d'Arcy <cmdarcy89@gmail.com>
2025-02-25 06:59:42 -0500

    add propTypes to contact row component

--- app/components/ContactRow.jsx
+++ app/components/ContactRow.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 import { useContacts } from "../contexts/contactsContext";
+import PropTypes from "prop-types";
 
 function ContactRow({ name, email, imgURL, phoneNum, id }) {
   const router = useRouter();
@@ -26,5 +27,13 @@ function ContactRow({ name, email, imgURL, phoneNum, id }) {
     </div>
   );
 }
+
+ContactRow.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+  imgURL: PropTypes.string,
+  phoneNum: PropTypes.string,
+  id: PropTypes.number,
+};
 
 export default ContactRow;
