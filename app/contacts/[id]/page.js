301cbd48e4c7afccaedb0ef992cd7dae63e514c1
Christopher d'Arcy <cmdarcy89@gmail.com>
2025-02-26 13:57:52 -0500

    add intial style dynamic contact page, add fallback image

--- app/contacts/[id]/page.js
+++ app/contacts/[id]/page.js
@@ -7,6 +7,7 @@ import { useContacts } from '@/app/contexts/contactsContext';
 
 function Contact() {
   const { id } = useParams();
+  const fallbackImageURL = '/defaultProfileIconSmall.png';
   const { getContact } = useContacts();
   const contact = getContact(parseInt(id));
 
@@ -17,13 +18,22 @@ function Contact() {
   const { name, email, imgURL, phoneNum } = contact;
 
   return (
-    <div>
-      <h1>
-        Contact {id}: {name}
-      </h1>
-      <Link href="/contacts"> Back </Link>
-      <img src={imgURL} alt="contact" />
-      <p>Email:{email}</p>
+    <div className="container text-center">
+      <h1>{name}</h1>
+      <Link className="btn btn-info" href="/contacts">
+        Back to Contacts
+      </Link>
+      <img
+        className="mx-auto d-block"
+        src={imgURL}
+        alt="contact"
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src = fallbackImageURL;
+          e.target.height = 200;
+        }}
+      />
+      <p>Email: {email}</p>
       <p>Phone Number: {phoneNum}</p>
     </div>
   );
